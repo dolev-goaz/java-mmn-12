3a252ecdda15638e549ccf57125df115790cc830
dolev goaz <www.dolev@gmail.com>
2024-04-09 21:29:00 +0300

    execute comparisons

--- src/q2/Main.java
+++ src/q2/Main.java
@@ -3,6 +3,9 @@ package q2;
 import java.util.Scanner;
 
 public class Main {
+    private static Character[] comparisons = {'=', '>', '<'};
+    private static Character[] operations = {'+', '-', '*', '/'};
+
     public static void main(String[] args) {
         System.out.println("Input rational number r1");
         Rational r1 = inputRational();
@@ -16,11 +19,46 @@ public class Main {
 
         System.out.println(); // separator
 
+        runComparisons(r1, r2);
+
+        System.out.println(); // separator
+
         runOperations(r1, r2);
     }
 
+    private static void runComparisons(Rational r1, Rational r2) {
+        for (Character comparison: comparisons) {
+            try {
+                boolean isComparisonTrue = runComparison(r1, r2, comparison);
+                String comparisonStr = isComparisonTrue
+                        ? "true"
+                        : "false";
+                System.out.println(
+                        String.format("%s %c %s is %s", r1, comparison, r2, comparisonStr)
+                );
+            } catch (UnsupportedOperationException e) {
+                System.err.println(e.getMessage());
+            }
+        }
+
+    }
+
+    private static boolean runComparison(Rational r1, Rational r2, Character comparisonChar) {
+        switch (comparisonChar) {
+            case '=':
+                return r1.equals(r2);
+            case '>':
+                return r1.greaterThan(r2);
+            case '<':
+                return r2.greaterThan(r1);
+            default:
+                throw new UnsupportedOperationException(
+                        String.format("Invalid rational comparison '%c'", comparisonChar)
+                );
+        }
+    }
+
     private static void runOperations(Rational r1, Rational r2) {
-        Character[] operations = {'+', '-', '*', '/'};
         for (Character operation: operations) {
             try {
                 Rational output = runOperation(r1, r2, operation);
