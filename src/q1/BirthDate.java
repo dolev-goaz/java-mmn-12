e467784e84fbec81b22c89a293bbb1c0d0773172
dolev goaz <www.dolev@gmail.com>
2024-04-09 23:44:40 +0300

    validate date, throws error if invalid

--- src/q1/BirthDate.java
+++ src/q1/BirthDate.java
@@ -7,9 +7,29 @@ public class BirthDate {
     private final int year;
 
     public BirthDate(int day, int month, int year) {
+        if (!isDateValid(day, month, year)) {
+            throw new IllegalArgumentException(
+                    String.format("Invalid date provided! Date- %d/%d/%d", day, month, year)
+            );
+        }
         this.day = day;
         this.month = month;
         this.year = year;
+    }
+
+    private static boolean isDateValid(int day, int month, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setLenient(false); // strict date checking
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month - 1); // month starts from 0
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        try {
+            calendar.getTime(); // throws an exception if date was invalid
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 
     @Override
