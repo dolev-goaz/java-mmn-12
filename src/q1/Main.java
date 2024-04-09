d86c4fdc49ab31f1bb059fff9a13aaaceed17d5f
dolev goaz <www.dolev@gmail.com>
2024-04-09 23:05:59 +0300

    print info + salary including birthday bonus

--- src/q1/Main.java
+++ src/q1/Main.java
@@ -1,17 +1,23 @@
 package q1;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 
 public class Main {
     private static final int MONTHS_IN_YEAR = 12;
+    private static final int BIRTH_MONTH_GIFT = 200;
+
     public static void main(String[] args) {
         ArrayList<Employee> employees = initializeEmployees();
 
         for (Employee employee : employees) {
-            // TODO: add 200₪
-            System.out.println(employee + "\n");
+            double salary = employee.earnings();
+            if (employee.getBirthDate().isBirthdayMonth()) {
+                salary += BIRTH_MONTH_GIFT;
+            }
+
+            System.out.println(String.format("%s%nSalary- %,.2f$", employee, salary));
+            System.out.println(); // separator
         }
     }
 
@@ -28,15 +34,15 @@ public class Main {
         ArrayList<Employee> employees = new ArrayList<Employee>();
         employees.add(new SalariedEmployee(
                 "Dani", "Kushmaro",
-                "111-21-3333", birthdays[3], 500));
+                "111-21-3333", birthdays[2], 500));
 
         employees.add(new CommissionEmployee(
                 "Micky", "Buganim",
-                "123-45-6789", birthdays[4], 50000, 0.2));
+                "123-45-6789", birthdays[3], 50000, 0.2));
 
         employees.add(new BasePlusCommissionEmployee(
                 "Dolev", "Goaz",
-                "987-65-4321", birthdays[5], 50000, 0.2, 20000));
+                "987-65-4321", birthdays[4], 50000, 0.2, 20000));
 
         employees.add(new HourlyEmployee(
                 "Nicki", "Minaj",
