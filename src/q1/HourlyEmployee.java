224f5f620464bc4f62e9fb4523bdbd22482b7e1b
dolev-goaz <www.dolev@gmail.com>
2024-04-09 15:10:41 +0300

    copy src

--- src/q1/HourlyEmployee.java
+++ src/q1/HourlyEmployee.java
@@ -2,71 +2,91 @@ package q1;
 
 // Fig. 10.6: HourlyEmployee.java
 // HourlyEmployee class extends Employee.
+
 public class HourlyEmployee extends Employee {
-    private double wage; // wage per hour
-    private double hours; // hours worked for week
+   private double wage; // wage per hour
+   private double hours; // hours worked for week
 
-    // constructor
-    public HourlyEmployee(String firstName, String lastName,
-                          String socialSecurityNumber, double wage, double hours) {
-        super(firstName, lastName, socialSecurityNumber);
+   // constructor
+   public HourlyEmployee(String firstName, String lastName,
+                         String socialSecurityNumber, double wage, double hours) {
+      super(firstName, lastName, socialSecurityNumber);
 
-        if (wage < 0.0) { // validate wage
-            throw new IllegalArgumentException("Hourly wage must be >= 0.0");
-        }
+      if (wage < 0.0) { // validate wage
+         throw new IllegalArgumentException("Hourly wage must be >= 0.0");
+      }
 
-        if ((hours < 0.0) || (hours > 168.0)) { // validate hours
-            throw new IllegalArgumentException(
-                    "Hours worked must be >= 0.0 and <= 168.0");
-        }
-        this.wage = wage;
-        this.hours = hours;
-    }
+      if ((hours < 0.0) || (hours > 168.0)) { // validate hours
+         throw new IllegalArgumentException(
+                 "Hours worked must be >= 0.0 and <= 168.0");
+      }
 
-    // set wage
-    public void setWage(double wage) {
-        if (wage < 0.0) { // validate wage
-            throw new IllegalArgumentException("Hourly wage must be >= 0.0");
-        }
+      this.wage = wage;
+      this.hours = hours;
+   }
 
-        this.wage = wage;
-    }
+   // set wage
+   public void setWage(double wage) {
+      if (wage < 0.0) { // validate wage
+         throw new IllegalArgumentException("Hourly wage must be >= 0.0");
+      }
 
-    // return wage
-    public double getWage() {
-        return wage;
-    }
+      this.wage = wage;
+   }
 
-    // set hours worked
-    public void setHours(double hours) {
-        if ((hours < 0.0) || (hours > 168.0)) { // validate hours
-            throw new IllegalArgumentException(
-                    "Hours worked must be >= 0.0 and <= 168.0");
-        }
+   // return wage
+   public double getWage() {
+      return wage;
+   }
 
-        this.hours = hours;
-    }
+   // set hours worked
+   public void setHours(double hours) {
+      if ((hours < 0.0) || (hours > 168.0)) { // validate hours
+         throw new IllegalArgumentException(
+                 "Hours worked must be >= 0.0 and <= 168.0");
+      }
 
-    // return hours worked
-    public double getHours() {
-        return hours;
-    }
+      this.hours = hours;
+   }
 
-    // calculate earnings; override abstract method earnings in Employee
-    @Override
-    public double earnings() {
-        if (getHours() <= 40) { // no overtime
-            return getWage() * getHours();
-        } else {
-            return 40 * getWage() + (getHours() - 40) * getWage() * 1.5;
-        }
-    }
+   // return hours worked
+   public double getHours() {
+      return hours;
+   }
 
-    // return String representation of HourlyEmployee object
-    @Override
-    public String toString() {
-        return String.format("hourly employee: %s%n%s: $%,.2f; %s: %,.2f",
-                super.toString(), "hourly wage", getWage(),
-                "hours worked", getHours());
-    }
+   // calculate earnings; override abstract method earnings in Employee
+   @Override
+   public double earnings() {
+      if (getHours() <= 40) { // no overtime
+         return getWage() * getHours();
+      } else {
+         return 40 * getWage() + (getHours() - 40) * getWage() * 1.5;
+      }
+   }
+
+   // return String representation of HourlyEmployee object
+   @Override
+   public String toString() {
+      return String.format("hourly employee: %s%n%s: $%,.2f; %s: %,.2f",
+              super.toString(), "hourly wage", getWage(),
+              "hours worked", getHours());
+   }
 }
+
+
+
+
+/**************************************************************************
+ * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
